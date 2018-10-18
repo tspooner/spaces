76c810f0510e531923f03b2a2f58c194bcaefa6a
Tom Spooner <spooner10000@gmail.com>
2018-10-18 17:14:23 +0100

    feat(continuous): add positive and non-negative real numbers

--- src/continuous/mod.rs
+++ src/continuous/mod.rs
@@ -1,2 +1,4 @@
 import_all!(reals);
 import_all!(interval);
+import_all!(positive_reals);
+import_all!(nonnegative_reals);
---
+++ src/continuous/nonnegative_reals.rs
@@ -0,0 +1,32 @@
+use core::{BoundedSpace, Space, Card, Surjection};
+use rand::Rng;
+
+/// A continous dimension.
+#[derive(Clone, Copy, Serialize)]
+pub struct NonNegativeReals;
+
+impl Space for NonNegativeReals {
+    type Value = f64;
+
+    fn dim(&self) -> usize { 1 }
+
+    fn card(&self) -> Card { Card::Infinite }
+
+    fn sample<R: Rng + ?Sized>(&self, _: &mut R) -> f64 {
+        unimplemented!()
+    }
+}
+
+impl BoundedSpace for NonNegativeReals {
+    type BoundValue = Self::Value;
+
+    fn inf(&self) -> Option<f64> { Some(0.0) }
+
+    fn sup(&self) -> Option<f64> { None }
+
+    fn contains(&self, val: Self::BoundValue) -> bool { val >= 0.0 }
+}
+
+impl Surjection<f64, f64> for NonNegativeReals {
+    fn map(&self, val: f64) -> f64 { val.max(0.0) }
+}
