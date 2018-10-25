40ca7d07d7450dc7bd2e6081a2fc23dc53b6a504
Tom Spooner <spooner10000@gmail.com>
2018-10-25 16:16:55 +0100

    feat(discrete): add NonZeroIntegers type

--- src/discrete/integers.rs
+++ src/discrete/integers.rs
@@ -24,3 +24,27 @@ impl BoundedSpace for Integers {
 
     fn contains(&self, _: Self::BoundValue) -> bool { true }
 }
+
+/// Type representing the set of non-zero integers, Z*.
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct NonZeroIntegers;
+
+impl Space for NonZeroIntegers {
+    type Value = i64;
+
+    fn dim(&self) -> usize { 1 }
+
+    fn card(&self) -> Card { Card::Infinite }
+
+    fn sample<R: Rng + ?Sized>(&self, _: &mut R) -> i64 { unimplemented!() }
+}
+
+impl BoundedSpace for NonZeroIntegers {
+    type BoundValue = Self::Value;
+
+    fn inf(&self) -> Option<i64> { None }
+
+    fn sup(&self) -> Option<i64> { None }
+
+    fn contains(&self, val: Self::BoundValue) -> bool { val != 0 }
+}
