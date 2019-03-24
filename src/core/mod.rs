7bab8f997ac633bf43033ada2336506cde4eb946
Tom Spooner <spooner10000@gmail.com>
2019-03-24 21:49:21 +0000

    feat(core): add mutable variants of VectorView and MatrixView

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -15,10 +15,12 @@ pub use self::enclosing::*;
 /// 1d array type.
 pub type Vector<T = f64> = ndarray::Array1<T>;
 pub type VectorView<'a, T = f64> = ndarray::ArrayView1<'a, T>;
+pub type VectorViewMut<'a, T = f64> = ndarray::ArrayViewMut1<'a, T>;
 
 /// 2d array type.
 pub type Matrix<T = f64> = ndarray::Array2<T>;
 pub type MatrixView<'a, T = f64> = ndarray::ArrayView2<'a, T>;
+pub type MatrixViewMut<'a, T = f64> = ndarray::ArrayViewMut2<'a, T>;
 
 mod interval;
 pub use self::interval::*;
